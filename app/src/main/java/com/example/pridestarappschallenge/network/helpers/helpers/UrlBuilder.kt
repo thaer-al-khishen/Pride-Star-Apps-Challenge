73314ce5a6bc2500021ac7eac44f6d4d1eada410
Thaer Al Khishen <thaer.alkhishen@technologysarl.com>
2021-02-06 12:04:54 +0200

    Create UrlBuilder class

---
+++ app/src/main/java/com/example/pridestarappschallenge/network/helpers/helpers/UrlBuilder.kt
@@ -0,0 +1,10 @@
+package com.example.pridestarappschallenge.network.helpers.helpers
+
+import com.example.pridestarappschallenge.BuildConfig
+
+object UrlBuilder {
+    const val BASE_URL = BuildConfig.baseUrl
+
+    //home
+    const val GET_ALL_HOME = "home.json"
+}
