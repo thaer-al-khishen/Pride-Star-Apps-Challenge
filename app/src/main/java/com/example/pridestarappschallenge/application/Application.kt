3267ba2c268e4b11a236e29cece78649ca7efc8e
Thaer Al Khishen <thaer.alkhishen@technologysarl.com>
2021-02-06 11:50:15 +0200

    Build application class and import in manifest

---
+++ app/src/main/java/com/example/pridestarappschallenge/application/Application.kt
@@ -0,0 +1,9 @@
+package com.example.pridestarappschallenge.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application(){
+
+}
